54a84a7baa384fba4683889100d8e3a3d53f1232
minjikimkim2222 <olivia0929@naver.com>
2024-06-23 21:56:33 +0900

    0623_유저프로필 사진 업로드,제거

--- src/main/java/org/myblog/domain/user/controller/UserController.java
+++ src/main/java/org/myblog/domain/user/controller/UserController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.myblog.domain.user.domain.User;
 import org.myblog.domain.user.service.UserService;
+import org.myblog.web.file.FileStore;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final FileStore fileStore;
+
     @GetMapping("/users/{id}/username")
     public String updateUsernameForm(@PathVariable Long id, Model model){
         User user = userService.findById(id);
@@ -86,5 +91,42 @@ public class UserController {
         userService.deleteUser(id);
 
         return ResponseEntity.ok().build();
+    }
+
+    // 프로필 이미지 삭제
+    @DeleteMapping("/users/{id}/profile-image")
+    public String deleteUserProfileImage(@PathVariable Long id){
+        User user = userService.findById(id);
+
+        log.info("User : {}", user);
+
+        if (user.getProfileFile() == null){ // 이미 프로필 사진이 없음
+            log.warn("User {} has no profile image to delete.", id);
+        } else {
+            // 프로필 사진이 있는 경우, 파일 삭제를 할 것
+            String filePath = fileStore.getFullPath(user.getProfileFile().getStoreFileName());
+            log.info("filePath : {}", filePath);
+            boolean isDeleted = deleteFile(filePath);
+
+            if (isDeleted){
+                user.setProfileFile(null); // 프로필 이미지를 null로 설정
+                userService.saveUser(user); // DB에 유저 정보 update !!
+            } else {
+                log.error("Failed to delete profile image for user {}", id);
+            }
+        }
+
+        return "redirect:/settings";
+    }
+
+    private boolean deleteFile(String filePath){
+        File file = new File(filePath);
+
+        if (file.exists()){
+            return file.delete();
+        } else {
+            log.warn("File {} does not exist. so, cannot delete", filePath);
+            return false;
+        }
     }
 }
